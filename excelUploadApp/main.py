99ef742fce8217649a5a8f71bee62a7ab1252d90
alaskar <asfkol@gmail.com>
2019-08-25 21:08:58 +0530

    excelUploadApp-added default config option

--- excelUploadApp/main.py
+++ excelUploadApp/main.py
@@ -18,39 +18,23 @@ logging.basicConfig(
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, appsettings):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.file_name = ""
         self.dir_name = ""
+        # set initialization variables.
+        self.excel_enabled = appsettings['enableexcel']
+        self.csv_enabled = appsettings['enablecsv']
+        self.delimiter = appsettings['delimiter']
+        self.header = appsettings['columns']
         # set up operations
         self.pushButton_browse.clicked.connect(self.openfile_dialog)
         self.pushButton_close.clicked.connect(self.close_window)
         self.pushButton_ok.clicked.connect(self.process_file)
-        # Initialize the application.
-        config = ConfigParser()
-        config.read('appSettings.ini')
-        options = config.options('Coreappsetting')
-        print(options)
-        for option in options:
-            try:
-                if option == 'delimiter':
-                    self.delimiter = self.config.get('Coreappsetting', option)
-                elif option == 'enableexcel':
-                    print(self.excel_enabled)
-                    excel_enabled = self.config.getboolean('Coreappsetting', option)
-                elif option == 'enablecsv':
-                    csv_enabled = self.config.getboolean('Coreappsetting', option)
-                elif option == 'columns':
-                    self.header = self.config.get('Coreappsetting', option)
-                elif option == -1:
-                    logging.info('Option %s is not enabled.' % option)
-            except Exception:
-                logging.critical('Error while reading config', exc_info=True)
-
         # enable disable based on settings
-        self.radioButton_excl.setEnabled(excel_enabled)
-        self.radioButton_csv.setEnabled(csv_enabled)
+        self.radioButton_excl.setEnabled(self.excel_enabled)
+        self.radioButton_csv.setEnabled(self.csv_enabled)
 
     def openfile_dialog(self):
         try:
@@ -75,46 +59,79 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def process_file(self):
         pass
 
-    def app_settings(self):
-        pass
+
+def app_settings(section):
+    appsetting = {}
+    config = ConfigParser()
+    if not path.exists('appSettings.ini'):
+        config.read('defaultSetting.ini')
+    else:
+        config.read('appSettings.ini')
+    options = config.options(section)
+    for option in options:
+        try:
+            if option == 'delimiter':
+                appsetting[option] = config.get(section, option)
+            elif option == 'enableexcel':
+                appsetting[option] = config.getboolean(section, option)
+            elif option == 'enablecsv':
+                appsetting[option] = config.getboolean(section, option)
+            elif option == 'columns':
+                appsetting[option] = config.get(section, option)
+            elif option == -1:
+                logging.info('Option %s is not enabled.' % option)
+        except Exception:
+            logging.critical('Error while reading config', exc_info=True)
+
+    return appsetting
 
 
 def main():
-    excel_obj = CsvFileCreator('testfile.xlsx')
-    config_dict = excel_obj.configsectionmap('Coreappsetting')
-    if config_dict:
-        logging.info('Application settings:')
-        logging.info('Delimiter: %s, '
-                     'EnableExcel: %s,'
-                     'EnableCSV: %s'
-                     % (config_dict['delimiter'],
-                        config_dict['enableexcel'],
-                        config_dict['enablecsv']))
-        delimiter = config_dict['delimiter']
-        print(type(delimiter))
-        enableexcel = config_dict['enableexcel']
-        enablecsv = config_dict['enablecsv']
-        header = config_dict['columns']
-    else:
-        logging.warning('Failed to read configuration, will reset to default values.')
-        delimiter = ','
-        enableexcel = True
-        enablecsv = True
-        header = None
 
-    parsed_data = excel_obj.parse_excel()
-    if not excel_obj.write_csv('Extracted.csv', parsed_data, delimiter, header):
-        print('Failed To Write csv')
-    else:
-        print('CSV File Generated')
+    # initialize the application
+    app_config = app_settings('Coreappsetting')
+    app = QApplication(sys.argv)
+    app.setApplicationName('Excel Converter/Uploader')
+    app.setStyle('Windows')
+    window = MainWindow(app_config)
+    window.show()
+    sys.exit(app.exec_())
+
+    # excel_obj = CsvFileCreator('testfile.xlsx')
+    # config_dict = excel_obj.configsectionmap('Coreappsetting')
+    # if config_dict:
+    #     logging.info('Application settings:')
+    #     logging.info('Delimiter: %s, '
+    #                  'EnableExcel: %s,'
+    #                  'EnableCSV: %s'
+    #                  % (config_dict['delimiter'],
+    #                     config_dict['enableexcel'],
+    #                     config_dict['enablecsv']))
+    #     delimiter = config_dict['delimiter']
+    #     print(type(delimiter))
+    #     enableexcel = config_dict['enableexcel']
+    #     enablecsv = config_dict['enablecsv']
+    #     header = config_dict['columns']
+    # else:
+    #     logging.warning('Failed to read configuration, will reset to default values.')
+    #     delimiter = ','
+    #     enableexcel = True
+    #     enablecsv = True
+    #     header = None
+    #
+    # parsed_data = excel_obj.parse_excel()
+    # if not excel_obj.write_csv('Extracted.csv', parsed_data, delimiter, header):
+    #     print('Failed To Write csv')
+    # else:
+    #     print('CSV File Generated')
 
 
 if __name__ == '__main__':
     import sys
-    app = QApplication(sys.argv)
-    app.setApplicationName('Excel Converter/Uploader')
-    app.setStyle('Windows')
-    window = MainWindow()
-    window.show()
-    # main()
-    sys.exit(app.exec_())
+    # app = QApplication(sys.argv)
+    # app.setApplicationName('Excel Converter/Uploader')
+    # app.setStyle('Windows')
+    # window = MainWindow()
+    # window.show()
+    main()
+    # sys.exit(app.exec_())
