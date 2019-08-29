82212acc33de1695e796cf27c981310fb2642046
alaskar <asfkol@gmail.com>
2019-08-29 11:31:32 +0530

    added regular expression for phone number.

--- regularexpressions/isPhoneNumber.py
+++ regularexpressions/isPhoneNumber.py
@@ -7,6 +7,29 @@ print(phoneNumReg.findall(message)[1])
 '''
 \d - numeric digit from 0-9
 \D any character that is  non-numeric i.e. 0-9
-
-
 '''
+
+# The dot character
+noNewlineRegex = re.compile(r'.*')  # this will match everything except newline
+mo = noNewlineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law.')
+
+print(mo.group())
+
+# remember the term DOTALL
+# The dot character which will match newline
+noNewlineRegex = re.compile(r'.*', re.DOTALL)  # this will match everything
+mo = noNewlineRegex.search('Serve the public trust.\nProtect the innocent.\nUphold the law.')
+
+print(mo.group())
+
+regex1 = re.compile('ROBOCOP', re.IGNORECASE)
+regex2 = re.compile('robocop', re.IGNORECASE)
+mo=regex1.findall(' ROBOCOP or robocop')
+print(mo)
+
+# replace using sub()
+agentNamesRegex = re.compile(r'Agent (\w)\w*')
+changedString = agentNamesRegex.sub(r'\1****', 'Agent Alice told Agent Carol that Agent '
+                                                'Eve knew Agent Bob was a double agent.')
+print(changedString)
+# 
--- regularexpressions/phoneNumberemailAddressExtractor.py
+++ regularexpressions/phoneNumberemailAddressExtractor.py
@@ -1,14 +1,29 @@
 import pyperclip
 import re
-phoneNumRegex = re.compile(r'''(
-(\d{3}|\(\d{3}\))?      # area code, 0 or more occurance
-(\s|-|\.)?              # separator
-\d{3}                   # first 3 digits
-(\s|-|\.)?              # separator
-\d{4}                   # next 4 digits
-(\s*(ext|x|ext.)\s*\d{2,5})? #extension
-)''', re.VERBOSE)
-matches=phoneNumEmailRegex.findall(textOffClipoard)
+# phoneNumRegex = re.compile(r'''(
+# (\d{3}|\(\d{3}\))?      # area code, 0 or more occurance
+# (\s|-|\.)?              # separator
+# \d{3}                   # first 3 digits
+# (\s|-|\.)?              # separator
+# \d{4}                   # next 4 digits
+# (\s*(ext|x|ext.)\s*\d{2,5})? #extension
+# )''', re.VERBOSE)
+phoneNumRegex = re.compile(r'''
+(
+  (\d{3})
+  (-?)
+  (\d{3})
+  (-)?
+  (\d{4})
+)
+''', re.VERBOSE)
+
+matches = phoneNumRegex.search('My numbers 7003305607 or 700-330-5607')
 print(matches)
 
-# 345/(456)
+# # 345/(456)
+# 7003305607
+# 07003305607
+# +91 7003305607
+
+
