c07436c05f21361764f57e0a527466dfbc928a39
Loneranger001 <asfkol@gmail.com>
2019-09-23 10:36:10 +0530

    connected to Gmail.

--- sendingmail/sendingmail.py
+++ sendingmail/sendingmail.py
@@ -3,9 +3,13 @@ import smtplib, ssl
 
 smtp_server = 'smtp.gmail.com'
 port = 465
+# Password1$
 
-sender = ''
+sender = 'myownpythondeveloper@gmail.com'
 password = input('Enter your password : ')
-# create a context
+# create a context, create default context
 context = ssl.create_default_context()
 
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender, password)
+    print('It worked!')
